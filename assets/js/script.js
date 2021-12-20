869773ed71fbbf5955ebee79dba2c5d5d8833855
Mayuran <mayuranswee23@gmail.com>
2021-12-19 23:56:08 -0500

    add darg and drop features

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,7 +75,73 @@ var taskP = $("<p>").addClass("m-1").text(text);
 $(this).replaceWith(taskP)
 });
 
+//sort lists by reordering or transfer to another section 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    // console.log("activate", this);
+  },
+  deactivate: function(event) {
+    // console.log("deactivate", this);
+  },
+  over: function(event) {
+    // console.log("over", event.target);
+  },
+  out: function(event) {
+    // console.log("out", event.target);
+  },
+  update: function(event) {
+    // array to store the task data in
+    var tempArr = [];
 
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+    
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+    
+      //add task data into the temp array 
+      tempArr.push({
+        text: text, 
+        date: date
+      });
+    });
+    // trim down list's ID to match object property
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+  
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+//to delete tasks using the trash sections draggable UI
+$("#trash").droppable({
+  accept: ".card .list-group-item", 
+  tolerance: "touch", 
+  drop: function(event, ui){
+    ui.draggable.remove();
+    console.log("drop");
+  }, 
+  over: function(event, ui){
+    // console.log("over");
+  }, 
+  out: function(event, ui){
+    // console.log("out");
+  }
+});
+  
 
 //to edit the date in the span 
 // due date was clicked
